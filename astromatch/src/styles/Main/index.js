15e94a62f3e5fa2bbe881a9f8697e4add4b50d86
DevAntunes <gabriel.antunes@acad.pucrs.br>
2022-06-06 15:14:30 -0300

    outras modificacoes

--- astromatch/src/styles/Main/index.js
+++ astromatch/src/styles/Main/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Container from './styles'
 
-const Layout=()=>{
+const Main=()=>{
   return (
    <Container>
 
@@ -9,4 +9,4 @@ const Layout=()=>{
   );
 }
 
-export default Layout;
+export default Main;
